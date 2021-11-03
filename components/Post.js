a462c1b24acb9741e3cff19a8505b53f1706345c
Ameno Murakumo <you@example.com>
2021-11-03 14:54:20 +0900

    Chang to function

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Post = ({ post }) => {
+export default function Post({ post }) {
   return (
     <li>
       <span>{post.id}</span>
@@ -12,6 +12,4 @@ const Post = ({ post }) => {
       </Link>
     </li>
   );
-};
-
-export default Post;
+}
